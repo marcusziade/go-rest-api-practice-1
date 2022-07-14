c6385e98cc0591d89782dbe2914b35d723f81d90
Marcus Ziadé <marcus.ziade@ouraring.com>
2022-07-14 17:11:13 +0300

    Add basic App structure

---
+++ main.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+// App - the struct which contains things like
+// pointers to database connections
+type App struct{}
+
+// Run - handles the startup of our application
+func (app *App) Run() error {
+	fmt.Println("Setting Up Our App")
+	return nil
+}
+
+// Our main entrypoint for the application
+func main() {
+	app := App{}
+	if err := app.Run(); err != nil {
+		fmt.Println("Error Starting Up")
+		fmt.Println(err)
+	}
+
+	println("App started")
+}
